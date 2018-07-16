6c031a93f6672b0440e6152830a9c0c0862e6960
506578835@qq.com <506578835@qq.com>
2018-07-16 10:37:47 +0800

    修改错误

--- app/src/main/java/com/fycx/demo/utils/MainActivity.java
+++ app/src/main/java/com/fycx/demo/utils/MainActivity.java
@@ -3,11 +3,14 @@ package com.fycx.demo.utils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.fycx.utils.StatusBarUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StatusBarUtils.setStatusBarLightMode(this);
     }
 }
